03ca61eee5ecfc0ed5bfce5213421b2feda6243b
M. Faris shafwan <farisbos.mfs@gmail.com>
2023-01-02 22:35:36 +0700

    update

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -9,13 +9,18 @@ use App\Models\User;
 use App\Models\City;
 use App\Models\PhotoProfile;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
     public function photo(Request $request){
         try{
+
+            $profile = Profile::firstWhere('user_id', auth()->user()->id);
+            $photoProfile = PhotoProfile::firstWhere('profile_id',$profile->id);
             $data = (object) [
                 'original' => $request->file,
+                'profile_id' => $profile->id,
                 'name' => $request->file->getClientOriginalName(),
                 'extension' => $request->file->getClientOriginalExtension(),
                 'is_image' => substr($request->file->getMimeType(), 0, 5) == 'image' ? true : false,
@@ -26,12 +31,39 @@ class ProfileController extends Controller
                 return $this->getResponse([],'File harus berupa .jpg/.jpeg/.png',422);
             }
 
-            $profile = Profile::firstWhere('user_id', auth()->user()->id);
-            $photoProfile = PhotoProfile::firstWhere('profile_id',$profile->id);
-
             if(!$photoProfile){
-                // $request-
+                $create = PhotoProfile::create((array) $data);
+                if(!$create){
+                    return $this->getResponse([],'Foto gagal disimpan.',500);
+                }
+                $save = $request->file->storeAs('profile', $data->name);
+                if(!$save){
+                    $create->forceDelete();
+                    return $this->getResponse([],'Foto gagal disimpan.');
+                }
+
+                return $this->getResponse(User::with(['profile','profile.photo_profile'])->find(auth()->user()->id),'Foto berhasil disimpan');
             }
+
+            $delete = Storage::disk('public')->delete('profile/'.$photoProfile->name);
+
+            if(!$delete){
+                return $this->getResponse([],'Foto gagal diperbarui.',500);
+            }
+
+            $update = $photoProfile->update((array) $data);
+
+            if(!$update){
+                return $this->getResponse([],'Foto gagal diperbarui.',500);
+            }
+
+            $replace = $request->file->storeAs('profile', $data->name);
+
+            if(!$replace){
+                return $this->getResponse([],'Foto gagal diperbarui.',500);
+            }
+
+            return $this->getResponse(User::with(['profile','profile.photo_profile'])->find(auth()->user()->id),'Foto berhasil disimpan');
         }
         catch(\Exception $e){
             return $this->getResponse([],$e->getMessage(),500);
